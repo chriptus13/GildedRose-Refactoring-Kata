6163b728eaab3dbda2c341c0c2e3560749dc077d
Chriptus <amcmmm13@hotmail.com>
2020-06-30 22:40:59 +0100

    refactor to a switch case based on item name

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,11 +9,21 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    ++item.quality;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            switch (item.name) {
+                case "Aged Brie":
+                    if (item.quality < 50) {
+                        ++item.quality;
+                    }
+                    --item.sellIn;
+                    if (item.sellIn < 0) {
+                        if (item.quality < 50) {
+                            ++item.quality;
+                        }
+                    }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (item.quality < 50) {
+                        ++item.quality;
                         if (item.sellIn < 11 && item.quality < 50) {
                             ++item.quality;
                         }
@@ -22,27 +32,23 @@ class GildedRose {
                             ++item.quality;
                         }
                     }
-                }
-            } else if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                --item.quality;
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                --item.sellIn;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            --item.quality;
-                        }
-                    } else {
+                    --item.sellIn;
+                    if (item.sellIn < 0) {
                         item.quality = 0;
                     }
-                } else if (item.quality < 50) {
-                    ++item.quality;
-                }
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
+                    if (item.quality > 0) {
+                        --item.quality;
+                    }
+                    --item.sellIn;
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
+                            --item.quality;
+                        }
+                    }
             }
         }
     }
